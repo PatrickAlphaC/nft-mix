126c7b8c69c09f8db50339b2d2f522fc3125b252
PatrickAlphaC <54278053+PatrickAlphaC@users.noreply.github.com>
2021-03-31 10:11:10 -0400

    removed creation with deployment

--- scripts/simple_collectible/deploy_simple.py
+++ scripts/simple_collectible/deploy_simple.py
@@ -10,7 +10,4 @@ def main():
     dev = accounts.add(os.getenv(config['wallets']['from_key']))
     print(network.show_active())
     publish_source = True if os.getenv("ETHERSCAN_TOKEN") else False
-    simple_collectible = SimpleCollectible.deploy(
-        {'from': dev}, publish_source=publish_source)
-    simple_collectible.createCollectible("None")
-    print(simple_collectible.ownerOf(0))
+    SimpleCollectible.deploy({'from': dev}, publish_source=publish_source)
