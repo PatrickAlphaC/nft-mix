f3cf268e4a7af1ffdbcd5eac090794face5ae6ab
snow <75382121+snowyaya@users.noreply.github.com>
2022-03-29 12:59:08 -0400

    Update upload_to_pinata.py
    put the code under `main()`

--- scripts/upload_to_pinata.py
+++ scripts/upload_to_pinata.py
@@ -14,9 +14,12 @@ headers = {'pinata_api_key': os.getenv('PINATA_API_KEY'),
            'pinata_secret_api_key': os.getenv('PINATA_API_SECRET')}
 
 
-with Path(filepath).open("rb") as fp:
-    image_binary = fp.read()
-    response = requests.post(PINATA_BASE_URL + endpoint,
-                             files={"file": (filename, image_binary)},
-                             headers=headers)
-    print(response.json())
+def main():
+    with Path(filepath).open("rb") as fp:
+        image_binary = fp.read()
+        response = requests.post(
+            PINATA_BASE_URL + endpoint,
+            files={"file": (filename, image_binary)},
+            headers=headers,
+        )
+        print(response.json())
