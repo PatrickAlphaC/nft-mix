e51f56ed76102e694a9c82694c2aa3000338e913
ReasonZx <xico.reasonzx@gmail.com>
2023-08-07 09:05:57 +0200

    Sepolia and VRFCoordinatorV2 adaptations

--- contracts/AdvancedCollectible.sol
+++ contracts/AdvancedCollectible.sol
@@ -1,53 +1,55 @@
-pragma solidity 0.6.6;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-contract AdvancedCollectible is ERC721, VRFConsumerBase {
+contract AdvancedCollectible is VRFConsumerBaseV2, ERC721URIStorage {
     uint256 public tokenCounter;
     enum Breed{PUG, SHIBA_INU, ST_BERNARD}
     // add other things
-    mapping(bytes32 => address) public requestIdToSender;
-    mapping(bytes32 => string) public requestIdToTokenURI;
+    mapping(uint256 => address) public requestIdToSender;
+    mapping(uint256 => string) public requestIdToTokenURI;
     mapping(uint256 => Breed) public tokenIdToBreed;
-    mapping(bytes32 => uint256) public requestIdToTokenId;
-    event RequestedCollectible(bytes32 indexed requestId); 
+    mapping(uint256 => uint256) public requestIdToTokenId;
+    event RequestedCollectible(uint256 indexed requestId); 
     // New event from the video!
-    event ReturnedCollectible(bytes32 indexed requestId, uint256 randomNumber);
+    event ReturnedCollectible(uint256 indexed newItemId, Breed breed);
 
 
     bytes32 internal keyHash;
-    uint256 internal fee;
+    uint64 subscriptionId;
+    VRFCoordinatorV2Interface COORDINATOR;
     
-    constructor(address _VRFCoordinator, address _LinkToken, bytes32 _keyhash)
-    public 
-    VRFConsumerBase(_VRFCoordinator, _LinkToken)
+    constructor(address _VRFCoordinator, uint64 _subscriptionId, bytes32 _keyhash) 
+    VRFConsumerBaseV2(_VRFCoordinator)
     ERC721("Dogie", "DOG")
     {
         tokenCounter = 0;
         keyHash = _keyhash;
-        fee = 0.1 * 10 ** 18;
+        COORDINATOR = VRFCoordinatorV2Interface(_VRFCoordinator);
+        subscriptionId = _subscriptionId;
     }
 
-    function createCollectible(string memory tokenURI) 
-        public returns (bytes32){
-            bytes32 requestId = requestRandomness(keyHash, fee);
+    function createCollectible(string memory tokenURI) public returns (uint256) {
+            uint256 requestId = COORDINATOR.requestRandomWords(keyHash, subscriptionId, 3, 1000000, 1);
             requestIdToSender[requestId] = msg.sender;
             requestIdToTokenURI[requestId] = tokenURI;
             emit RequestedCollectible(requestId);
     }
 
-    function fulfillRandomness(bytes32 requestId, uint256 randomNumber) internal override {
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomNumber) internal override {
         address dogOwner = requestIdToSender[requestId];
         string memory tokenURI = requestIdToTokenURI[requestId];
         uint256 newItemId = tokenCounter;
         _safeMint(dogOwner, newItemId);
         _setTokenURI(newItemId, tokenURI);
-        Breed breed = Breed(randomNumber % 3); 
+        Breed breed = Breed(randomNumber[0] % 3); 
         tokenIdToBreed[newItemId] = breed;
         requestIdToTokenId[requestId] = newItemId;
         tokenCounter = tokenCounter + 1;
-        emit ReturnedCollectible(requestId, randomNumber);
+        emit ReturnedCollectible(newItemId, breed);
     }
 
     function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.6.6;
+pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract SimpleCollectible is ERC721 {
+contract SimpleCollectible is ERC721URIStorage {
     uint256 public tokenCounter;
     constructor () public ERC721 ("Dogie", "DOG"){
         tokenCounter = 0;
